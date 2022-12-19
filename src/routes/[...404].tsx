8a40f79ca8197d81d3a017dfbc2b8dfc44ed517a
J.B.C <juanbicortelezzi@gmail.com>
2022-12-19 05:23:42 -0300

    feat 404

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -2,10 +2,14 @@ import { A } from "solid-start";
 
 export default function NotFound() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Not Found
-      </h1>
+    <main class="lg:container mx-auto text-gray-700 my-5 space-y-4">
+      <h1 class="text-6xl text-primary font-bold">Oops...</h1>
+      <h2 class="text-3xl text-secondary-content ">
+        We could not find that page.{" "}
+        <A href="/" class="link-accent">
+          Wanna go home?
+        </A>
+      </h2>
     </main>
   );
 }
